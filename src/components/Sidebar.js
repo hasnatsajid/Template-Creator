5e3dd828baf4ddb2baf1538b6b3c026f51c22599
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-07-05 14:49:01 +0500

    sidebar added

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,32 @@
 const SideBar = () => {
-  return <div className="sidebar">sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar_item">
+        <img src="/assets/images/style.png" alt="" />
+        <h2>Styles</h2>
+      </div>
+      <div className="sidebar_item">
+        <img src="/assets/images/blocks.png" alt="" />
+        <h2>Blocks</h2>
+      </div>
+      <div className="sidebar_item">
+        <img src="/assets/images/pages.png" alt="" />
+        <h2>Pages</h2>
+      </div>
+      <div className="sidebar_item">
+        <img src="/assets/images/lang.png" alt="" />
+        <h2>Lang</h2>
+      </div>
+      <div className="sidebar_item">
+        <img src="/assets/images/style.png" alt="" />
+        <h2>Modes</h2>
+      </div>
+      <div className="sidebar_item">
+        <img src="/assets/images/delete.png" alt="" />
+        <h2>Remove</h2>
+      </div>
+    </div>
+  );
 };
 
 export default SideBar;
