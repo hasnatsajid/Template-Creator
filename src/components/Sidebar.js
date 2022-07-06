e8a697f415b92c0192a3818cb1ba32996e9aaceb
hasnatsajid965 <86589885+hasnatsajid@users.noreply.github.com>
2022-07-06 13:55:17 +0500

    added react-beautiful-dnd library

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,30 +1,87 @@
+import { Link } from 'react-router-dom';
+import { Draggable, Droppable, DragDropContext } from 'react-beautiful-dnd';
+import { useState } from 'react';
+
 const SideBar = () => {
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    if (result.destination.index === result.source.index) return;
+    const projects = reorder(
+      project, // project is state
+      result.source.index,
+      result.destination.index
+    );
+    //store reordered state.
+    setProject(projects);
+  }; // outside the component
+
+  const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+    return result;
+  };
+
+  const Arr = [
+    {
+      name: 'Styles',
+      key: '12353',
+      img: '/assets/images/style.png',
+    },
+    {
+      name: 'Blocks',
+      key: '23463',
+      img: '/assets/images/blocks.png',
+    },
+    {
+      name: 'Pages',
+      key: '54643',
+      img: '/assets/images/pages.png',
+    },
+    {
+      name: 'Lang',
+      key: '4435',
+      img: '/assets/images/lang.png',
+    },
+    {
+      name: 'Modes',
+      key: '4235',
+      img: '/assets/images/style.png',
+    },
+    {
+      name: 'Remove',
+      key: '4245',
+      img: '/assets/images/delete.png',
+    },
+  ];
+
+  const [project, setProject] = useState(Arr);
+
   return (
     <div className="sidebar">
-      <div className="sidebar_item">
-        <img src="/assets/images/style.png" alt="" />
-        <h2>Styles</h2>
-      </div>
-      <div className="sidebar_item">
-        <img src="/assets/images/blocks.png" alt="" />
-        <h2>Blocks</h2>
-      </div>
-      <div className="sidebar_item">
-        <img src="/assets/images/pages.png" alt="" />
-        <h2>Pages</h2>
-      </div>
-      <div className="sidebar_item">
-        <img src="/assets/images/lang.png" alt="" />
-        <h2>Lang</h2>
-      </div>
-      <div className="sidebar_item">
-        <img src="/assets/images/style.png" alt="" />
-        <h2>Modes</h2>
-      </div>
-      <div className="sidebar_item">
-        <img src="/assets/images/delete.png" alt="" />
-        <h2>Remove</h2>
-      </div>
+      <Droppable droppableId="list">
+        {(provided) => (
+          <div ref={provided.innerRef} {...provided.droppableProps}>
+            {project &&
+              project.map((item, index) => (
+                // draggableId should be string, index is also require, key props should be unique to prevent from unnecassary re-rendering
+                <Draggable draggableId={item.key} key={item.key} index={index}>
+                  {(provided) => (
+                    <div ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                      <Link to="/">
+                        <div className="sidebar_item">
+                          <img src={item.img} alt="" />
+                          <h2>{item.name}</h2>
+                        </div>
+                      </Link>
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,26 @@
 import SideBar from '../components/Sidebar';
+import { DragDropContext, Droppable } from 'react-beautiful-dnd';
 
 const Home = () => {
+  const onDragEnd = (result) => {
+    console.log(result);
+  };
+
   return (
     <div className="homePage">
       <div className="home">
-        <div className="home_sidebar">
-          <SideBar />
-        </div>
-        <div className="home_showcase"></div>
+        <DragDropContext onDragEnd={onDragEnd}>
+          <div className="home_sidebar">
+            <SideBar />
+          </div>
+          <Droppable droppableId="template">
+            {(provided) => (
+              <div className="home_showcase" ref={provided.innerRef} {...provided.droppableProps}>
+                hello
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
     </div>
   );
